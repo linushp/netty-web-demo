202c2cffca8e6a20942f68a38dccddc61ee63da9
luan <master@ubibi.cn>
2018-04-20 10:42:42 +0800

    字符集支持

--- src/main/java/com/github/luanhaipeng/nettyweb/test/controllers/UserController.java
+++ src/main/java/com/github/luanhaipeng/nettyweb/test/controllers/UserController.java
@@ -2,13 +2,14 @@ package com.github.luanhaipeng.nettyweb.test.controllers;
 
 import cn.ubibi.nettyweb.framework.rest.annotation.Controller;
 import cn.ubibi.nettyweb.framework.rest.annotation.GetMapping;
+import cn.ubibi.nettyweb.framework.rest.annotation.PathVariable;
 import cn.ubibi.nettyweb.framework.rest.annotation.RequestParam;
 
 @Controller("/user")
 public class UserController {
 
-    @GetMapping("/list")
-    public String getUserList(@RequestParam("name") String name){
+    @GetMapping("/list/:name")
+    public String getUserList(@PathVariable("name") String name){
         return "user list " + name;
     }
 
